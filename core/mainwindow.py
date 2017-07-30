23275790cb413d4c0f63a35ee884b1af5da73e33
Marmoth85 <projets.stats.info@gmail.com>
2017-07-30 04:07:46 +0200

    Implemented SIGNAL and SLOT for menu bar

--- core/mainwindow.py
+++ core/mainwindow.py
@@ -1,9 +1,24 @@
-from PyQt5 import QtWidgets
+import sys
 
-from gen_files import mainwindow
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
+from gen_files import ui_mainwindow
 
+class MainWindow(QMainWindow, ui_mainwindow.Ui_MainWindow):
+			
 	def __init__(self, parent=None):
 		super(MainWindow, self).__init__(parent)
 		self.setupUi(self)
+			
+	@pyqtSlot(QAction)	
+	def proceedAction(self, action):
+		# sender = self.sender()
+		if(action == self.actionShowEvents):
+			self.showOpenEvents()
+		# print(sender)
+		# print(action.text())
+	
+	def showOpenEvents(self):
+		QMessageBox.question(self, "Titre", "Message : Le menu a bien été cliqué, mais la fonction n'est pas encore implémentée", QMessageBox.Ok, QMessageBox.Ok)
